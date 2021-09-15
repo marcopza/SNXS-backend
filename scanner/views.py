78e9ba23cb1d671cce103735d0291de48e2409f7
Jose Ossorio <joseossorio99@hotmail.com>
2021-09-15 17:44:30 -0500

    set up dummy test view using django-rest-framework

--- scanner/views.py
+++ scanner/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response
 
 # Create your views here.
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "msg": "Hello, World!"
+        }
+        return Response(data)
