0dd9aefecd0155278e80f693e28ccbf13f71952f
Jose Ossorio <joseossorio99@hotmail.com>
2021-10-20 13:08:10 -0500

    rename project

--- SNXS/wsgi.py
+++ SNXS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EZSec project.
+WSGI config for SNXS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EZSec.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SNXS.settings')
 
 application = get_wsgi_application()
